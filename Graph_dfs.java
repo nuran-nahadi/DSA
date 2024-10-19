f7409c621bb1607321d87b017f167f67486a5c3c
nuran-nahadi <126339869+nuran-nahadi@users.noreply.github.com>
2024-10-20 01:33:46 +0600

    Update Graph_dfs.java
    Added topological sorting

--- Graph_dfs.java
+++ Graph_dfs.java
@@ -1,11 +1,18 @@
+import java.util.*;
+
+import java.io.File;
+
 class Graph {
-    public List<List<Integer>> adj_list;
+    private List<List<Integer>> adj_list;
     private Set<Integer> vertex;
+    private Stack<Integer> topology = new Stack<>();
     int[] prev;
-    int[] d;
+    int[] distance;
+    int[] bfs_state;
     int[] dfs_state;
     int[] disco;
     int[] fin;
+    int time = 0;
     int cap = 0;
 
     Graph(String filename) {
@@ -25,6 +32,7 @@ class Graph {
                 int v = sc.nextInt();
                 add_directed_edge(u, v);
             }
+            init();
             sc.close();
         }catch(Exception e)
             {
@@ -32,6 +40,23 @@ class Graph {
             }
     }
 
+    private void init(){
+        prev = new int[1000000];
+        distance = new int[1000000];
+        dfs_state = new int[1000000];
+        disco = new int[1000000];
+        fin = new int[1000000];
+        bfs_state = new int[1000000];
+
+        for (int i=0; i < cap; i++) {
+            dfs_state[i] = 0;
+            disco[i] = Integer.MAX_VALUE;
+            fin[i] = Integer.MAX_VALUE;
+            prev[i] = Integer.MIN_VALUE;
+        }
+
+    }
+    
     public void add_edge(int u, int v) {
 
         adj_list.get(u).add(v);
@@ -77,19 +102,19 @@ class Graph {
 
     public void BFS(int s) {
         Queue<Integer> q = new LinkedList<>();
-        int[] state = new int[cap];
+        //bfs_state = new int[cap];
 
-        d = new int[cap];
-        prev = new int[cap];
+        // distance = new int[cap];
+        // prev = new int[cap];
 
         for (int i=0; i < cap; i++) {
-            state[i] = 0;
-            d[i] = Integer.MAX_VALUE;
+            bfs_state[i] = 0;
+            distance[i] = Integer.MAX_VALUE;
             prev[i] = Integer.MIN_VALUE;
         }
 
-        state[s] = 1;
-        d[s] = 0;
+        bfs_state[s] = 1;
+        distance[s] = 0;
         prev[s] = s;
         q.add(s);
 
@@ -98,25 +123,26 @@ class Graph {
             // System.out.print(current+" ");
 
             for (int i : adj_list.get(current)) {
-                if (state[i] == 0) {
-                    state[i] = 1;
-                    d[i] = d[current] + 1;
+                if (bfs_state[i] == 0) {
+                    bfs_state[i] = 1;
+                    distance[i] = distance[current] + 1;
                     prev[i] = current;
                     q.add(i);
                 }
             }
-            state[current] = 2;
+            bfs_state[current] = 2;
         }
         System.out.println();
     }
 
 
     public void DFS(int s){
-        dfs_state = new int[cap];
+        
+        // dfs_state = new int[cap];
 
-        disco = new int[cap];
-        fin = new int[cap];
-        prev = new int[cap];
+        // disco = new int[cap];
+        // fin = new int[cap];
+        // prev = new int[cap];
 
         for (int i=0; i < cap; i++) {
             dfs_state[i] = 0;
@@ -124,12 +150,11 @@ class Graph {
             fin[i] = Integer.MAX_VALUE;
             prev[i] = Integer.MIN_VALUE;
         }
-        int time = 0;
         System.out.print("DFS traversal starting from vertex "+ s +":");
-        DFS_visit(s,time);
+        DFS_visit(s);
     }
 
-    public void DFS_visit(int u,int time){
+    public void DFS_visit(int u){
         dfs_state[u] = 1;
         time++;
         disco[u] = time;
@@ -137,15 +162,43 @@ class Graph {
         for (int i : adj_list.get(u)) {
             if (dfs_state[i] == 0) {
                 prev[i] = u;
-                DFS_visit(i, time);
+                DFS_visit(i);
             }
         }
         dfs_state[u] = 2;
         time++;
         fin[u] = time;
+        topology.push(u);
     }
 
-
+    private void dfsTraversal(int u){
+        dfs_state[u] = 1;
+        time++;
+        disco[u] = time;
+        for (int i : adj_list.get(u)) {
+            if (dfs_state[i] == 0) {
+                prev[i] = u;
+                DFS_visit(i);
+            }
+        }
+        dfs_state[u] = 2;
+        time++;
+        fin[u] = time;
+        topology.push(u);
+    }
+    public List<Integer> topologicalSort(){
+        for(int i : vertex){
+            if (dfs_state[i] == 0) {
+                dfsTraversal(i);
+            }
+            
+        }
+        List<Integer> toporder = new LinkedList<>();
+        while (!topology.empty()) {
+            toporder.add(topology.pop());
+        }
+        return toporder;
+    }
 
 
 
@@ -165,19 +218,19 @@ class Graph {
 
     public void BFS_path(int s) {
         Queue<Integer> q = new LinkedList<>();
-        int[] state = new int[cap];
+        int[] bfs_state = new int[cap];
 
-        d = new int[cap];
+        distance = new int[cap];
         prev = new int[cap];
 
         for (int i=0; i < cap; i++) {
-            state[i] = 0;
-            d[i] = Integer.MAX_VALUE;
+            bfs_state[i] = 0;
+            distance[i] = Integer.MAX_VALUE;
             prev[i] = Integer.MIN_VALUE;
         }
 
-        state[s] = 1;
-        d[s] = 0;
+        bfs_state[s] = 1;
+        distance[s] = 0;
         prev[s] = s;
         q.add(s);
 
@@ -186,14 +239,14 @@ class Graph {
             System.out.print(current + " ");
 
             for (int i : adj_list.get(current)) {
-                if (state[i] == 0) {
-                    state[i] = 1;
-                    d[i] = d[current] + 1;
+                if (bfs_state[i] == 0) {
+                    bfs_state[i] = 1;
+                    distance[i] = distance[current] + 1;
                     prev[i] = current;
                     q.add(i);
                 }
             }
-            state[current] = 2;
+            bfs_state[current] = 2;
         }
         System.out.println();
     }
@@ -241,5 +294,23 @@ class Graph {
                 System.out.println();
             }
         }
+    }
+}
+
+public class Graph_file {
+
+    public static void main(String[] args) {
+        Graph graph = new Graph("dinput.txt");
+
+        System.out.println("Graph adjacency list:");
+        graph.displayGraph();
+
+        System.out.println("\nPerforming DFS starting from vertex 5:");
+        graph.DFS(5);
+
+        System.out.println("\nPerforming Topological Sort:");
+        List<Integer> topoOrder = graph.topologicalSort();
+        System.out.println("Topological Sort order: " + topoOrder);
+
     }
 }
